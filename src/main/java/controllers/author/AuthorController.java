fa66869b386d7e3411a574b5c95ac5b3027a56d6
unknown <juanmagarrocho@gmail.com>
2019-07-11 19:48:46 +0200

    Controladores sin save y forms

--- src/main/java/controllers/author/AuthorController.java
+++ src/main/java/controllers/author/AuthorController.java
@@ -1,10 +1,52 @@
 package controllers.author;
 
 
+import forms.AuthorForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import services.ActorService;
+import services.AuthorService;
 
 @Controller
 @RequestMapping("author")
 public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @Autowired
+    private ActorService actorService;
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView create() {
+
+        ModelAndView result;
+        AuthorForm authorForm;
+        authorForm = new AuthorForm();
+        result = this.createEditModelAndView(authorForm);
+
+        return result;
+    }
+
+    //TODO: Save
+
+    protected ModelAndView createEditModelAndView(final AuthorForm authorForm) {
+        ModelAndView result;
+        result = this.createEditModelAndView(authorForm, null);
+        return result;
+    }
+
+    protected ModelAndView createEditModelAndView(final AuthorForm authorForm, final String messageCode) {
+
+        final ModelAndView result;
+
+        result = new ModelAndView("author/register");
+        result.addObject("authorForm", authorForm);
+        result.addObject("message", messageCode);
+
+        return result;
+    }
 }
--- src/main/java/controllers/sponsor/SponsorController.java
+++ src/main/java/controllers/sponsor/SponsorController.java
@@ -1,9 +1,55 @@
 package controllers.sponsor;
 
+import domain.Sponsor;
+import forms.SponsorForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import services.ActorService;
+import services.SponsorService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("sponsor")
 public class SponsorController {
+
+    @Autowired
+    private SponsorService sponsorService;
+
+    @Autowired
+    private ActorService actorService;
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView create() {
+
+        ModelAndView result;
+        SponsorForm sponsorForm;
+        sponsorForm = new SponsorForm();
+        result = this.createEditModelAndView(sponsorForm);
+
+        return result;
+    }
+
+    //TODO: Save
+
+    protected ModelAndView createEditModelAndView(final SponsorForm sponsorForm) {
+        ModelAndView result;
+        result = this.createEditModelAndView(sponsorForm, null);
+        return result;
+    }
+
+    protected ModelAndView createEditModelAndView(final SponsorForm sponsorForm, final String messageCode) {
+
+        final ModelAndView result;
+
+        result = new ModelAndView("sponsor/register");
+        result.addObject("sponsorForm", sponsorForm);
+        result.addObject("message", messageCode);
+
+        return result;
+    }
 }
