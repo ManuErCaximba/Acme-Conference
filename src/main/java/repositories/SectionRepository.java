04003e0ad4b75240d3c2019b3b1527f91f326565
unknown <juanmagarrocho@gmail.com>
2019-07-23 11:28:14 +0200

    Repositorios

---
+++ src/main/java/repositories/SectionRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import domain.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectionRepository extends JpaRepository<Section, Integer> {
+}
