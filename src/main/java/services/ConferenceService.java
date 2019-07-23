4b66e155aad88000811c9c9a3ebae0c47bd95b1c
unknown <juanmagarrocho@gmail.com>
2019-07-23 19:27:33 +0200

    Save Conference

--- src/main/java/services/ConferenceService.java
+++ src/main/java/services/ConferenceService.java
@@ -36,12 +36,34 @@ public class ConferenceService {
 
     public Conference saveFinal(Conference conference){
         Assert.notNull(conference);
+        Conference res;
         if(conference.getId() == 0){
             Administrator admin = this.administratorService.findOne(this.actorService.getActorLogged().getId());
             conference.setAdministrator(admin);
         }
         conference.setFinal(true);
+        Assert.isTrue(conference.getAdministrator().getId() == this.actorService.getActorLogged().getId());
+        Assert.isTrue(conference.getSubmissionDeadline().before(conference.getNotificationDeadline()));
+        Assert.isTrue(conference.getNotificationDeadline().before(conference.getCameraReadyDeadline()));
+        Assert.isTrue(conference.getCameraReadyDeadline().before(conference.getStartDate()));
+        Assert.isTrue(conference.getStartDate().before(conference.getEndDate()));
+        res = this.conferenceRepository.save(conference);
+        return res;
+    }
 
+    //TODO:Revisar
+    public Conference saveDraft(Conference conference){
+        Assert.notNull(conference);
+        Assert.isTrue(conference.isFinal() == false);
+        Conference res;
+        if(conference.getId() == 0){
+            Administrator admin = this.administratorService.findOne(this.actorService.getActorLogged().getId());
+            conference.setAdministrator(admin);
+        }
+        conference.setFinal(false);
+        Assert.isTrue(conference.getAdministrator().getId() == this.actorService.getActorLogged().getId());
+        res = this.conferenceRepository.save(conference);
+        return res;
     }
 
     public Collection<Conference> findAll(){
