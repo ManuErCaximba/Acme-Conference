769102809fc6a33931be09a1690cfb6c5746d1cd
unknown <juanmagarrocho@gmail.com>
2019-07-23 18:06:48 +0200

    Empezando servicio conference

--- src/main/java/services/ConferenceService.java
+++ src/main/java/services/ConferenceService.java
@@ -1,5 +1,7 @@
 package services;
 
+import domain.Actor;
+import domain.Administrator;
 import domain.Conference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,31 @@ public class ConferenceService {
     //Managed Repositories
     @Autowired
     private ConferenceRepository conferenceRepository;
+
+    //Supporting services
+    @Autowired
+    private ActorService actorService;
+
+    @Autowired
+    private AdministratorService administratorService;
+
+    public Conference create(){
+        final Actor actor = this.actorService.getActorLogged();
+        Assert.isTrue(actor.getUserAccount().getAuthorities().iterator().next().getAuthority().equals("ADMIN"));
+
+        Conference res = new Conference();
+        return res;
+    }
+
+    public Conference saveFinal(Conference conference){
+        Assert.notNull(conference);
+        if(conference.getId() == 0){
+            Administrator admin = this.administratorService.findOne(this.actorService.getActorLogged().getId());
+            conference.setAdministrator(admin);
+        }
+        conference.setFinal(true);
+
+    }
 
     public Collection<Conference> findAll(){
         Collection<Conference> res;
