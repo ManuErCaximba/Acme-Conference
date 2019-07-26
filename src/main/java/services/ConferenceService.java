02bfa744b071d6137978f6c1b5ceaa2bf49a53a2
unknown <juanmagarrocho@gmail.com>
2019-07-26 13:36:41 +0200

    Siguiendo con ConferenceService

--- src/main/java/services/ConferenceService.java
+++ src/main/java/services/ConferenceService.java
@@ -6,9 +6,12 @@ import domain.Conference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import repositories.ConferenceRepository;
 
 import javax.transaction.Transactional;
+import javax.validation.ValidationException;
 import java.util.Collection;
 
 @Service
@@ -26,6 +29,9 @@ public class ConferenceService {
     @Autowired
     private AdministratorService administratorService;
 
+    @Autowired
+    private Validator validator;
+
     public Conference create(){
         final Actor actor = this.actorService.getActorLogged();
         Assert.isTrue(actor.getUserAccount().getAuthorities().iterator().next().getAuthority().equals("ADMIN"));
@@ -35,6 +41,8 @@ public class ConferenceService {
     }
 
     public Conference saveFinal(Conference conference){
+        final Actor actor = this.actorService.getActorLogged();
+        Assert.isTrue(actor.getUserAccount().getAuthorities().iterator().next().getAuthority().equals("ADMIN"));
         Assert.notNull(conference);
         Conference res;
         if(conference.getId() == 0){
@@ -42,7 +50,6 @@ public class ConferenceService {
             conference.setAdministrator(admin);
         }
         conference.setFinal(true);
-        Assert.isTrue(conference.getAdministrator().getId() == this.actorService.getActorLogged().getId());
         Assert.isTrue(conference.getSubmissionDeadline().before(conference.getNotificationDeadline()));
         Assert.isTrue(conference.getNotificationDeadline().before(conference.getCameraReadyDeadline()));
         Assert.isTrue(conference.getCameraReadyDeadline().before(conference.getStartDate()));
@@ -51,8 +58,10 @@ public class ConferenceService {
         return res;
     }
 
-    //TODO:Revisar
+
     public Conference saveDraft(Conference conference){
+        final Actor actor = this.actorService.getActorLogged();
+        Assert.isTrue(actor.getUserAccount().getAuthorities().iterator().next().getAuthority().equals("ADMIN"));
         Assert.notNull(conference);
         Assert.isTrue(conference.isFinal() == false);
         Conference res;
@@ -61,9 +70,23 @@ public class ConferenceService {
             conference.setAdministrator(admin);
         }
         conference.setFinal(false);
-        Assert.isTrue(conference.getAdministrator().getId() == this.actorService.getActorLogged().getId());
+        Assert.isTrue(conference.getSubmissionDeadline().before(conference.getNotificationDeadline()));
+        Assert.isTrue(conference.getNotificationDeadline().before(conference.getCameraReadyDeadline()));
+        Assert.isTrue(conference.getCameraReadyDeadline().before(conference.getStartDate()));
+        Assert.isTrue(conference.getStartDate().before(conference.getEndDate()));
         res = this.conferenceRepository.save(conference);
         return res;
+    }
+
+    public void delete (Conference conference){
+        final Actor actor = this.actorService.getActorLogged();
+        Assert.isTrue(actor.getUserAccount().getAuthorities().iterator().next().getAuthority().equals("ADMIN"));
+
+        Assert.notNull(conference);
+        Assert.isTrue(conference.getId() != 0);
+        Assert.isTrue(conference.isFinal() == false);
+
+        this.conferenceRepository.delete(conference);
     }
 
     public Collection<Conference> findAll(){
@@ -77,5 +100,36 @@ public class ConferenceService {
         final Conference res = this.conferenceRepository.findOne(conferenceId);
         Assert.notNull(res);
         return res;
+    }
+
+    //Reconstruct TODO: Actualizo Admin?
+    public Conference reconstruct(Conference conference, BindingResult binding){
+        Conference result;
+        if (conference.getId() == 0){
+            result = this.create();
+        } else {
+            result = this.conferenceRepository.findOne(conference.getId());
+        }
+        result.setTitle(conference.getTitle());
+        result.setAcronym(conference.getAcronym());
+        result.setVenue(conference.getVenue());
+        result.setSubmissionDeadline(conference.getSubmissionDeadline());
+        result.setNotificationDeadline(conference.getNotificationDeadline());
+        result.setCameraReadyDeadline(conference.getCameraReadyDeadline());
+        result.setStartDate(conference.getStartDate());
+        result.setEndDate(conference.getEndDate());
+        result.setSummary(conference.getSummary());
+        result.setFee(conference.getFee());
+        result.setFinal(conference.isFinal());
+        result.setCategory(conference.getCategory());
+        result.setRegistrations(conference.getRegistrations());
+        result.setComments(conference.getComments());
+
+        validator.validate(result, binding);
+
+        if (binding.hasErrors()){
+            throw new ValidationException();
+        }
+        return result;
     }
 }
